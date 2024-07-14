b3c3cc27101397c4c633ce8669f62ce5cadea7b3
Miksen <mikkel.linddk@gmail.com>
2024-07-14 17:15:02 +0200

    Deploy website - based on b05a4bafe4ebfaaefaf0da43bd8c1d454cc135c8

--- src/components/Cards/Cards.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Cards.module.css';
-
-const Cards = ({ children }) => {
-  return <div className={styles.Cards}>{children}</div>;
-};
-
-export default Cards;
