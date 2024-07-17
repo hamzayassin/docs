64a2f5bf0e49df6ceb47f126579fc50b6fc01218
Miksen <mikkel.linddk@gmail.com>
2024-07-17 10:35:31 +0200

    hm

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'fshostme', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
-  trailingSlash: true,
+  trailingSlash: false,
 
   //onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
